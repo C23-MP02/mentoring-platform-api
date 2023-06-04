a56714d6662d96512b4f31ed8c2a41bf12bf6b3e
Fahrel <fahrel@students.undip.ac.id>
2023-06-04 22:22:27 +0700

    chore: removing unnecessary console.log

--- src/services/mentee.service.ts
+++ src/services/mentee.service.ts
@@ -2,35 +2,40 @@ import APIRepository from "../repositories/api.repository";
 import MentorRepository from "../repositories/mentor.repository";
 import UserRepository from "../repositories/user.repository";
 
-export class MenteeService{
-    private apiRespository: APIRepository;
-    private mentorRepository: MentorRepository;
-    private userRepository: UserRepository;
+export class MenteeService {
+  private apiRespository: APIRepository;
+  private mentorRepository: MentorRepository;
+  private userRepository: UserRepository;
 
-    constructor(){
-        this.apiRespository = new APIRepository();
-        this.mentorRepository = new MentorRepository();
-        this.userRepository = new UserRepository();
-    }
+  constructor() {
+    this.apiRespository = new APIRepository();
+    this.mentorRepository = new MentorRepository();
+    this.userRepository = new UserRepository();
+  }
 
-    async getAllMentors(mentee_id: string) {
-        // TODO: Integrate with ML API
-        const mentors = await this.mentorRepository.getAllMentors();
-        const mentee = await this.userRepository.getUserById(mentee_id);
-        const data = {
-            mentee: mentee!,
-            mentors
-        }
-        
-        const matchmakingResult = await this.apiRespository.getMatchmakingResult(data);
+  async getAllMentors(mentee_id: string) {
+    const mentors = await this.mentorRepository.getAllMentors();
+    const mentee = await this.userRepository.getUserById(mentee_id);
+    const data = {
+      mentee: mentee!,
+      mentors,
+    };
 
-        const sortedMentors = mentors.sort((a, b) => {
-            const indexA = matchmakingResult.indexOf(a.user_id);
-            const indexB = matchmakingResult.indexOf(b.user_id);
-            return indexA - indexB;
-        });
-        return sortedMentors;
-      }
+    console.log(mentors);
+
+    const matchmakingResult = await this.apiRespository.getMatchmakingResult(
+      data
+    );
+
+    console.log(matchmakingResult);
+
+    const sortedMentors = mentors.sort((a, b) => {
+      const indexA = matchmakingResult.indexOf(a.user_id);
+      const indexB = matchmakingResult.indexOf(b.user_id);
+      return indexA - indexB;
+    });
+    return sortedMentors;
+  }
 }
 
 const menteeService = new MenteeService();
